e6f7247f2cdabf6f55630f3d8046fc7618467f19
link2x <link2x@users.noreply.github.com>
2022-11-10 00:19:52 -0500

    warn disabled release code (RC) access

--- pages/rc/[slug].js
+++ pages/rc/[slug].js
@@ -8,8 +8,8 @@ export default function Sorry() {
             <Paper sx={{p: '1em', my: '2em'}} >
                 <Typography variant='h4' textAlign={'center'} sx={{pb: '0.5em'}}>Sorry, this content is no longer available.</Typography>
                 <Typography variant='body1'>Hi there! Unfortunately, as part of a server move, this content is no longer available through these links.</Typography>
-                <Typography variant='body1'>I'm no longer hosting these files directly, but that doesn't mean you can't have them!</Typography>
-                <Typography variant='body1'>Feel free to email me at eden@link2x.us, and I'll be super happy to send you MP3s of any song.</Typography>
+                <Typography variant='body1'>I&apos;m no longer hosting these files directly, but that doesn&apos;t mean you can&apos;t have them!</Typography>
+                <Typography variant='body1'>Feel free to email me at eden@link2x.us, and I&apos;ll be super happy to send you MP3s of any song.</Typography>
             </Paper>
         </Container>
     )
