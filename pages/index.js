f36bd16d0902d1948ad4e619617457f546ce5667
link2x <link2x@users.noreply.github.com>
2023-07-11 00:02:57 -0400

    Link to IC Tool

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import * as React from 'react'
+
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+import { useRouter } from 'next/router';
+
 export default function Home() {
+
+    const router = useRouter()
+
     return(
-        <div>Broken link?</div>
+        <Container sx={{p: '2em'}}>
+            <Stack direction='row' alignContent={'center'} alignItems={'center'} justifyContent={'center'}>
+                <Stack direction='column' spacing={2}>
+                    <Typography variant='h4'>L2X.US Tools</Typography>
+                    <Button onClick={() => {router.push('https://ic.l2x.us/')}}>IC Safe Foods</Button>
+                </Stack>
+            </Stack>
+        </Container>
         )
 }
