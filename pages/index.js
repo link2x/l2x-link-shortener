b4a474eea2b41a1fe4427cf3844ef77e59b7f096
link2x <link2x@users.noreply.github.com>
2023-07-25 21:59:17 -0400

    More pretty

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,13 @@ import Stack from '@mui/material/Stack'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 import { useRouter } from 'next/router';
 
 function ToolButton(props) {
     return(
         <Tooltip title={props.description}>
-            <Button variant='outlined' color='primary' fullWidth onClick={() => {router.push(props.link)}}>{props.title}</Button>
+            <Button size='large' variant='outlined' color='primary' fullWidth onClick={() => {router.push(props.link)}}>{props.title}</Button>
         </Tooltip>
     )
 }
@@ -22,20 +23,33 @@ export default function Home() {
     const router = useRouter()
 
     return(
-        <Container sx={{p: '2em'}} maxWidth='xl'>
+        <Container component={Paper} sx={{p: '2em'}} maxWidth='xl'>
             <Grid container spacing={8} sx={{flexGrow: 1}} justifyContent={'center'}>
-                <Grid item xl={12}>
-                    <Paper elevation={4} sx={{px: '2em', pt: '7em', pb: '1em'}}>
-                        <Typography align='right' variant='h4'>L2X.US Tools</Typography>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Paper elevation={4}
+                        sx={{
+                            px: '2em',
+                            pt: '7em',
+                            pb: '1em',
+                            background: 'linear-gradient(129deg, rgba(29,63,84,1) 0%, rgba(3,26,38,1) 100%)'
+                        }}>
+                        <Typography
+                            align='right'
+                            variant='h4'
+                            sx={{
+                                background: 'linear-gradient(129deg, rgba(120,148,166,1) 0%, rgba(14,107,156,1) 100%)',
+                                backgroundClip: 'text',
+                                WebkitTextFillColor: 'transparent'}}>
+                            L2X.US Tools</Typography>
                     </Paper>
                 </Grid>
-                <Grid item xl={12} alignSelf={'center'} textAlign={'center'}>
+                <Grid item xs={12} md={6} lg={12} alignSelf={'center'} textAlign={'center'}>
                     <Typography variant='body'>
                         This site hosts a variety of publicly available tools in the form of web apps. <br/>
                         All tools are completely free unless otherwise noted, and many are open source.
                     </Typography>
                 </Grid>
-                <Grid item xl={6}>
+                <Grid item xs={12} md={6} lg={6}>
                     <Stack direction='column' spacing={2}>
                         <ToolButton
                             title='IC Safe Foods'
